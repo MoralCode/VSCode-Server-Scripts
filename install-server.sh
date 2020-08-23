d6b932545f1b95584a464f7460e76335b40f8502
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-08-23 14:34:54 -0700

    cleanup and better document server install instructions

--- install-server.sh
+++ install-server.sh
@@ -1,18 +1,5 @@
-# gcloud compute machine-types list
-# INSTANCE_TYPE="f1-micro"
-# gcloud compute images list
-# INSTANCE_IMAGE="debian-10"
-# INSTANCE_PROJECT="helloworld2020-servers"
-# gcloud compute zones list
-# INSTANCE_ZONE="us-west1-b"
-# INSTANCE_NAME=""
-# read -r -p 'Enter a name for this instance: ' INSTANCE_NAME
+# Step 1: set up the first code-server instance manually using the official instructions that may ot may not look like this:
 
-
-# setup compute instance
-# gcloud compute instances create "$INSTANCE_NAME" --image="$INSTANCE_IMAGE" --image-project="$INSTANCE_PROJECT" --machine-type="$INSTANCE_TYPE" --zone="$INSTANCE_ZONE";
-
-# exit(0);
 #install code-server
 
 # curl -fsSL https://code-server.dev/install.sh | sh
@@ -21,7 +8,7 @@
 #set up and run code-server
 # systemctl --user enable --now code-server
 # alow the server to run user level services after logout
-# sudo loginctl enable-linger USERNAME
+# sudo loginctl enable-linger `$whoami``
 
 # install caddy 
 # https://github.com/cdr/code-server/blob/master/doc/guide.md#lets-encrypt
@@ -30,3 +17,5 @@
 # sudo apt update
 # sudo apt install caddy
 
+
+# now you can create a disk image of this instance to replicate it many times
