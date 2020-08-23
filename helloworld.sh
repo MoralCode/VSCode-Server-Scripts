8580acd5469b4af88bb379702cf42003019cd930
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-08-23 14:17:52 -0700

    make helloworld.sh more reusable and well-documented

--- helloworld.sh
+++ helloworld.sh
@@ -1,22 +1,24 @@
+#!/bin/bash
+# This script is meant to be run on each instance upon setup to set instance-specific settings like caddy configuration and passwords
 
-
-# configure individual intances
-
+domainname="bitbybitcoding.org"
+vscode_bind_addr="0.0.0.0:8080"
 
 caddyfile="$(hostname):80, $( ping -q -c 1 $(hostname) | head -n 1 | cut -d " " -f 2
-):80, $(hostname).bitbybitcoding.org:80 {
+):80, $(hostname).$(domainname):80 {
 
         log {
         output file         /var/log/caddy/files-sample.log
         }
-        root * /home/ace/my-website
+        root * /home/$(whoami)/my-website
         file_server
 }
 "
 
 echo "$caddyfile" | sudo tee /etc/caddy/Caddyfile
-sudo mkdir -p /var/log/caddy/
-sudo chmod g+w /var/log/caddy
+#logging never ended up working right
+# sudo mkdir -p /var/log/caddy/
+# sudo chmod g+w /var/log/caddy
 
 sudo systemctl start caddy
 # sudo systemctl reload caddy
@@ -28,10 +30,8 @@ sudo chgrp caddy /var/log/caddy
 #user setup (with user perms)
 read -p "please enter the password you wish to set for accessing the code-server: " pass
 sed -i.bak "s/password: .*/password: $pass/" ~/.config/code-server/config.yaml
-sed -i.bak "s/bind-addr: .*/bind-addr: 0.0.0.0:8080 /" ~/.config/code-server/config.yaml
+sed -i.bak "s/bind-addr: .*/bind-addr: $vscode_bind_addr /" ~/.config/code-server/config.yaml
 mkdir -p ~/my-website
 
 systemctl --user restart code-server
 sudo systemctl restart caddy
-
-# sudo reboot
